47251cfeeb902f0772e0967f7f4a04d52fe863cb
suraj <suraj.kadam@niyuj.com>
2020-01-20 18:46:27 +0530

    v0.10 Caption changes...

--- collectors/gsuite/test/utils_test.js
+++ collectors/gsuite/test/utils_test.js
@@ -4,12 +4,12 @@ const utils = require("../utils");
 const gsuiteMock = require('./gsuite_mock');
 const { google, admin_reports_v1 } = require("googleapis");
 
-let serviceF, activityF, service;
+let mockServiceObject, mockActivityObject, service;
 describe('Unit Tests', function () {
 
     beforeEach(function () {
         service = new admin_reports_v1.Admin();
-        serviceF = sinon.stub(google, 'admin').callsFake(
+        mockServiceObject = sinon.stub(google, 'admin').callsFake(
             function fakeFn(path) {
                 return service;
             });
@@ -18,14 +18,14 @@ describe('Unit Tests', function () {
     });
 
     afterEach(function () {
-        serviceF.restore();
-        activityF.restore();
+        mockServiceObject.restore();
+        mockActivityObject.restore();
     });
 
     describe('List Event From Gsuite', function () {
         it('Check List Event Logs', function (done) {
 
-            activityF = sinon.stub(service.activities, 'list').callsFake(
+            mockActivityObject = sinon.stub(service.activities, 'list').callsFake(
                 function fakeFn(params) {
                     return new Promise(function (resolve, reject) {
                         let response = {
