35156c28686d712489f0cfc8da26b7837d1f4e8d
Joe Dragovich <joseph.dragovich@alertlogic.com>
2020-04-18 10:22:43 +0100

    fix time rounding an next page token issue

--- collectors/o365/o365_collector.js
+++ collectors/o365/o365_collector.js
@@ -85,10 +85,12 @@ class O365Collector extends PawsCollector {
         let collector = this;
         console.info(`O365000001 Collecting data from ${state.since} till ${state.until} for stream ${state.stream}`);
 
-        if(moment().diff(state.since, 'days') > 7){
+        if(moment().diff(state.since, 'days', true) > 7){
             const newStart = moment().subtract(PARTIAL_WEEK, 'days');
             state.since = newStart.toISOString();
             state.until = newStart.add(1, 'hours').toISOString();
+            // remove next page token if the state is out of date as well.
+            state.nextPage = null;
             console.warn(
                 "O365000005 Start timestamp is more than 7 days in the past. ",
                 "This is not allowed in the MS managment API. ",
