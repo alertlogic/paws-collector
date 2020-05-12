4c403409684ccfe8d8bcd46cf0add34404301386
JDragovichAlertLogic <47750771+JDragovichAlertLogic@users.noreply.github.com>
2020-05-12 14:56:35 +0100

    DD handler to all collectors (#117)
    * DD handler to all collectors
    
    * bump versions
    
    * bump sentinalone package.json. forgotton
    
    * bump version on the collectors for framework fix

--- collectors/carbonblack/index.js
+++ collectors/carbonblack/index.js
@@ -12,10 +12,10 @@ const debug = require('debug') ('index');
 
 const CarbonblackCollector = require('./collector').CarbonblackCollector;
 
-exports.handler = function(event, context) {
+exports.handler = CarbonblackCollector.makeHandler(function(event, context) {
     debug('input event: ', event);
     CarbonblackCollector.load().then(function(creds) {
         var carbonblackc = new CarbonblackCollector(context, creds);
         carbonblackc.handleEvent(event);
     });
-};
+});
--- collectors/sentinelone/index.js
+++ collectors/sentinelone/index.js
@@ -12,10 +12,10 @@ const debug = require('debug') ('index');
 
 const SentineloneCollector = require('./collector').SentineloneCollector;
 
-exports.handler = function(event, context) {
+exports.handler = SentineloneCollector.makeHandler(function(event, context) {
     debug('input event: ', event);
     SentineloneCollector.load().then(function(creds) {
         var sentinelonec = new SentineloneCollector(context, creds);
         sentinelonec.handleEvent(event);
     });
-};
+});
