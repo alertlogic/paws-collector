1c963046caadaab26601b6867dbd0b8c97feeb99
suraj <suraj.kadam@niyuj.com>
2020-05-14 19:33:56 +0530

    Added code coverage...

--- collectors/auth0/test/auth0_test.js
+++ collectors/auth0/test/auth0_test.js
@@ -8,46 +8,49 @@ const auth0Mock = require('./auth0_mock');
 var m_alCollector = require('@alertlogic/al-collector-js');
 var Auth0Collector = require('../auth0_collector').Auth0Collector;
 const m_al_aws = require('@alertlogic/al-aws-collector-js').Util;
+const utils = require("../utils");
 
 
 var alserviceStub = {};
 var responseStub = {};
 var setEnvStub = {};
 
+let getAPILogs;
+
 function setAlServiceStub() {
     alserviceStub.get = sinon.stub(m_alCollector.AlServiceC.prototype, 'get').callsFake(
         function fakeFn(path, extraOptions) {
-            return new Promise(function(resolve, reject) {
+            return new Promise(function (resolve, reject) {
                 var ret = null;
                 switch (path) {
                     case '/residency/default/services/ingest/endpoint':
                         ret = {
-                            ingest : 'new-ingest-endpoint'
-                    };
+                            ingest: 'new-ingest-endpoint'
+                        };
                         break;
-                case '/residency/default/services/azcollect/endpoint':
-                    ret = {
-                        azcollect : 'new-azcollect-endpoint'
-                    };
-                    break;
-                default:
-                    break;
+                    case '/residency/default/services/azcollect/endpoint':
+                        ret = {
+                            azcollect: 'new-azcollect-endpoint'
+                        };
+                        break;
+                    default:
+                        break;
                 }
                 return resolve(ret);
             });
         });
     alserviceStub.post = sinon.stub(m_alCollector.AlServiceC.prototype, 'post').callsFake(
-            function fakeFn(path, extraOptions) {
-                return new Promise(function(resolve, reject) {
-                    return resolve();
-                });
+        function fakeFn(path, extraOptions) {
+            return new Promise(function (resolve, reject) {
+                return resolve();
             });
+        });
     alserviceStub.del = sinon.stub(m_alCollector.AlServiceC.prototype, 'deleteRequest').callsFake(
-            function fakeFn(path) {
-                return new Promise(function(resolve, reject) {
-                    return resolve();
-                });
+        function fakeFn(path) {
+            return new Promise(function (resolve, reject) {
+                return resolve();
             });
+        });
 }
 
 function restoreAlServiceStub() {
@@ -57,7 +60,7 @@ function restoreAlServiceStub() {
 }
 
 function mockSetEnvStub() {
-    setEnvStub = sinon.stub(m_al_aws, 'setEnv').callsFake((vars, callback)=>{
+    setEnvStub = sinon.stub(m_al_aws, 'setEnv').callsFake((vars, callback) => {
         const {
             ingest_api,
             azcollect_api
@@ -73,21 +76,21 @@ function mockSetEnvStub() {
     });
 }
 
-describe('Unit Tests', function() {
+describe('Unit Tests', function () {
 
-    beforeEach(function(){
+    beforeEach(function () {
         AWS.mock('SSM', 'getParameter', function (params, callback) {
             const data = new Buffer('test-secret');
-            return callback(null, {Parameter : { Value: data.toString('base64')}});
+            return callback(null, { Parameter: { Value: data.toString('base64') } });
         });
-        
+
         AWS.mock('KMS', 'decrypt', function (params, callback) {
             const data = {
-                    Plaintext : 'decrypted-sercret-key'
+                Plaintext: 'decrypted-sercret-key'
             };
             return callback(null, data);
         });
-        
+
         responseStub = sinon.stub(m_response, 'send').callsFake(
             function fakeFn(event, mockContext, responseStatus, responseData, physicalResourceId) {
                 mockContext.succeed();
@@ -97,7 +100,7 @@ describe('Unit Tests', function() {
         mockSetEnvStub();
     });
 
-    afterEach(function(){
+    afterEach(function () {
         restoreAlServiceStub();
         setEnvStub.restore();
         responseStub.restore();
@@ -105,16 +108,16 @@ describe('Unit Tests', function() {
         AWS.restore('SSM');
     });
 
-    describe('pawsInitCollectionState', function() {
+    describe('pawsInitCollectionState', function () {
         let ctx = {
-            invokedFunctionArn : auth0Mock.FUNCTION_ARN,
-            fail : function(error) {
+            invokedFunctionArn: auth0Mock.FUNCTION_ARN,
+            fail: function (error) {
                 assert.fail(error);
             },
-            succeed : function() {}
+            succeed: function () { }
         };
-        it('sets up intial state', function(done) {
-            Auth0Collector.load().then(function(creds) {
+        it('sets up intial state', function (done) {
+            Auth0Collector.load().then(function (creds) {
                 const testPollInterval = 60;
                 var collector = new Auth0Collector(ctx, creds);
                 const startDate = moment().subtract(1, 'days').toISOString();
@@ -130,24 +133,102 @@ describe('Unit Tests', function() {
             });
         });
     });
-    describe('Format Tests', function() {
-        it('log format success', function(done) {
+    describe('Format Tests', function () {
+        it('log format success', function (done) {
             let ctx = {
-                invokedFunctionArn : auth0Mock.FUNCTION_ARN,
-                fail : function(error) {
+                invokedFunctionArn: auth0Mock.FUNCTION_ARN,
+                fail: function (error) {
                     assert.fail(error);
                     done();
                 },
-                succeed : function() {
+                succeed: function () {
                     done();
                 }
             };
-            
-            Auth0Collector.load().then(function(creds) {
+
+            Auth0Collector.load().then(function (creds) {
                 var collector = new Auth0Collector(ctx, creds);
                 let fmt = collector.pawsFormatLog(auth0Mock.AUTH0_LOG_EVENT);
                 assert.equal(fmt.progName, 'Auth0Collector');
                 assert.ok(fmt.messageTypeId);
+                done();
+            });
+        });
+    });
+
+    describe('pawsGetLogs', function () {
+        let ctx = {
+            invokedFunctionArn: auth0Mock.FUNCTION_ARN,
+            fail: function (error) {
+                assert.fail(error);
+            },
+            succeed: function () { }
+        };
+        it('Paws Get Logs Success', function (done) {
+            getAPILogs = sinon.stub(utils, 'getAPILogs').callsFake(
+                function fakeFn(auth0Client, state, accumulator, maxPagesPerInvocation) {
+                    return new Promise(function (resolve, reject) {
+                        return resolve({ accumulator: [auth0Mock.AUTH0_LOG_EVENT, auth0Mock.AUTH0_LOG_EVENT], nextLogId: "nextLogId", lastLogTs: null });
+                    });
+                });
+            Auth0Collector.load().then(function (creds) {
+                var collector = new Auth0Collector(ctx, creds);
+                const startDate = moment().subtract(3, 'days');
+                const curState = {
+                    since: startDate.toISOString(),
+                    poll_interval_sec: 1
+                };
+
+                collector.pawsGetLogs(curState, (err, logs, newState, newPollInterval) => {
+                    assert.equal(logs.length, 2);
+                    assert.equal(newState.poll_interval_sec, collector.pollInterval);
+                    assert.equal(newState.last_log_id, "nextLogId");
+                    assert.ok(logs[0].log_id);
+                    getAPILogs.restore();
+                    done();
+                });
+
+            });
+        });
+    });
+
+    describe('Next state tests', function () {
+        let ctx = {
+            invokedFunctionArn: auth0Mock.FUNCTION_ARN,
+            fail: function (error) {
+                assert.fail(error);
+            },
+            succeed: function () { }
+        };
+        it('log format success', function (done) {
+            Auth0Collector.load().then(function (creds) {
+                var collector = new Auth0Collector(ctx, creds);
+                const startDate = moment();
+                const curState = {
+                    since: startDate.toISOString(),
+                    poll_interval_sec: 1
+                };
+                let nextLogId = "nextLogId";
+                let lastLogTs = startDate.toISOString();
+                let nextState = collector._getNextCollectionState(curState, nextLogId, lastLogTs);
+                assert.equal(nextState.last_log_id, 'nextLogId');
+                assert.equal(nextState.poll_interval_sec, collector.pollInterval);
+                done();
+            });
+        });
+        it('log format success with nextLogId null', function (done) {
+            Auth0Collector.load().then(function (creds) {
+                var collector = new Auth0Collector(ctx, creds);
+                const startDate = moment();
+                const curState = {
+                    since: startDate.toISOString(),
+                    poll_interval_sec: 1
+                };
+                let nextLogId = null;
+                let lastLogTs = null;
+                let nextState = collector._getNextCollectionState(curState, nextLogId, lastLogTs);
+                assert.ok(nextState.since);
+                assert.equal(nextState.poll_interval_sec, 1);
                 done();
             });
         });
---
+++ collectors/auth0/test/utils_test.js
@@ -0,0 +1,64 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const utils = require("../utils");
+const moment = require('moment');
+const auth0Mock = require('./auth0_mock');
+
+let getLogsStub, auth0Client;
+
+describe('Unit Tests', function () {
+
+    beforeEach(function () {
+        auth0Client = {
+            getLogs: () => { }
+        };
+    });
+
+    describe('Get API Logs', function () {
+        it('Get API Logs', function (done) {
+            getLogsStub = sinon.stub(auth0Client, 'getLogs').callsFake(
+                function fakeFn() {
+                    return new Promise(function (resolve, reject) {
+                        return resolve([auth0Mock.AUTH0_LOG_EVENT]);
+                    });
+                });
+
+            const startDate = moment().subtract(5, 'minutes');
+            let state = {
+                since: startDate.toISOString(),
+                poll_interval_sec: 1
+            };
+            let maxPagesPerInvocation = 5;
+            let accumulator = [];
+
+            utils.getAPILogs(auth0Client, state, accumulator, maxPagesPerInvocation).then(data => {
+                assert(accumulator.length == 5, "accumulator length is wrong");
+                getLogsStub.restore();
+                done();
+            });
+        });
+    });
+
+    describe('Get API Logs with last log id', function () {
+        it('Get API Logs  with last log id', function (done) {
+            getLogsStub = sinon.stub(auth0Client, 'getLogs').callsFake(
+                function fakeFn() {
+                    return new Promise(function (resolve, reject) {
+                        return resolve([]);
+                    });
+                });
+            let state = {
+                last_log_id: "last_log_id",
+                poll_interval_sec: 1
+            };
+            let maxPagesPerInvocation = 5;
+            let accumulator = [];
+
+            utils.getAPILogs(auth0Client, state, accumulator, maxPagesPerInvocation).then(data => {
+                assert(accumulator.length == 0, "accumulator length is wrong");
+                getLogsStub.restore();
+                done();
+            });
+        });
+    });
+});
