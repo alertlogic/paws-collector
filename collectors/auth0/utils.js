d6eec46cc87f17fd43316f956191d766b380ede2
surajkadam <kadamsuraj5555@gmail.com>
2020-05-19 15:16:11 +0530

    Auth0 (Added pagination logic) (#116)
    * Added pagination logic
    
    * updated paws-collector version 1.2.0 to 1.3.0
    
    * increased per page limit 50 to 100
    
    * resolve conflict...
    
    * Added code coverage...
    
    * Updated package version from 1.3.1 to 1.3.2
    
    * Code changes...
    
    Co-authored-by: suraj <suraj.kadam@niyuj.com>

---
+++ collectors/auth0/utils.js
@@ -0,0 +1,40 @@
+function getAPILogs(auth0Client, state, accumulator, maxPagesPerInvocation) {
+    let pageCount = 0;
+
+    let params = state.last_log_id ? { from: state.last_log_id, take:100 } : { q: "date=[" + state.since + " TO *]", per_page:100, sort: "date:1" };
+    let nextLogId = state.last_log_id ? state.last_log_id : null;
+    let lastLogTs = null;
+
+    return new Promise(function (resolve, reject) {
+        getData(params);
+        function getData(params) {
+            if (pageCount < maxPagesPerInvocation) {
+                return auth0Client
+                    .getLogs(params)
+                    .then(function (logAcc) {
+                        pageCount++;
+                        if (logAcc.length > 0) {
+                            accumulator.push(...logAcc);
+                            nextLogId = logAcc[logAcc.length - 1].log_id;
+                            lastLogTs = logAcc[logAcc.length - 1].date;
+                            params = { from: nextLogId, take:100 };
+                            getData(params);
+                        }
+                        else {
+                            resolve({ accumulator, nextLogId, lastLogTs });
+                        }
+                    })
+                    .catch(function (err) {
+                        reject(err);
+                    });
+            }
+            else {
+                resolve({ accumulator, nextLogId, lastLogTs });
+            }
+        }
+    });
+}
+
+module.exports = {
+    getAPILogs: getAPILogs
+};
