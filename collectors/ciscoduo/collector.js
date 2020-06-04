b4db7f88c29eb54f0b03a103e292ec70d841cbdd
suraj <suraj.kadam@niyuj.com>
2020-06-04 15:12:28 +0530

    API integration changes...

--- collectors/ciscoduo/collector.js
+++ collectors/ciscoduo/collector.js
@@ -14,76 +14,154 @@ const PawsCollector = require('@alertlogic/paws-collector').PawsCollector;
 const parse = require('@alertlogic/al-collector-js').Parse;
 const packageJson = require('./package.json');
 const duo = require('@duosecurity/duo_api');
+const utils = require("./utils");
+const calcNextCollectionInterval = require('@alertlogic/paws-collector').calcNextCollectionInterval;
 
 
-const typeIdPaths = [
-    // enter your type paths in the form { path: ['myKey'] }
-];
+let typeIdPaths = [];
 
-const tsPaths = [
-    // enter your timestamp paths in the form { path: ['myKey'] }
-];
+let tsPaths = [];
+
+const Authentication = 'Authentication';
 
 
 class CiscoduoCollector extends PawsCollector {
     constructor(context, creds) {
         super(context, creds, packageJson.version);
     }
-    
+
     pawsInitCollectionState(event, callback) {
-        const startTs = process.env.paws_collection_start_ts ? 
-                process.env.paws_collection_start_ts :
-                    moment().toISOString();
+        const startTs = process.env.paws_collection_start_ts ?
+            process.env.paws_collection_start_ts :
+            moment().toISOString();
         const endTs = moment(startTs).add(this.pollInterval, 'seconds').toISOString();
-        const initialState = {
-            // TODO: define initial collection state specific to your collector.
-            // You will get this state back with each invocation
-            since: startTs,
-            until: endTs,
-            poll_interval_sec: 1
-        };
-        return callback(null, initialState, 1);
+        const objectNames = JSON.parse(process.env.paws_collector_param_string_1);
+        const initialStates = objectNames.map(object => {
+            if (object === Authentication) {
+                return {
+                    object,
+                    mintime: moment(startTs).valueOf(),
+                    maxtime: moment(endTs).valueOf(),
+                    nextPage: null,
+                    poll_interval_sec: 1
+                }
+            }
+            else {
+                return {
+                    object,
+                    mintime: moment(startTs).unix(),
+                    poll_interval_sec: 1
+                }
+            }
+        });
+        return callback(null, initialStates, 1);
     }
-    
+
     pawsGetLogs(state, callback) {
         let collector = this;
-        const hostname = collector.pawsDomainEndpoint;
-        console.info(`CDUO000001 Collecting data from ${state.since} till ${state.until}`);
-        const newState = collector._getNextCollectionState(state);
-        console.info(`CDUO000002 Next collection in ${newState.poll_interval_sec} seconds`);
-        return callback(null, [], newState, newState.poll_interval_sec);
-    }
-    
-    _getNextCollectionState(curState) {
-        const nowMoment = moment();
-        const curUntilMoment = moment(curState.until);
-        
-        // Check if current 'until' is in the future.
-        const nextSinceTs = curUntilMoment.isAfter(nowMoment) ?
-                nowMoment.toISOString() :
-                curState.until;
 
-        const nextUntilMoment = moment(nextSinceTs).add(this.pollInterval, 'seconds');
-        // Check if we're behind collection schedule and need to catch up.
-        const nextPollInterval = nowMoment.diff(nextUntilMoment, 'seconds') > this.pollInterval ?
-                1 : this.pollInterval;
-        
-        return  {
-            // TODO: define the next collection state.
-            // This needs to be in the smae format as the intial colletion state above
-             since: nextSinceTs,
-             until: nextUntilMoment.toISOString(),
-             poll_interval_sec: nextPollInterval
-        };
+        const clientSecret = collector.secret;
+        if (!clientSecret) {
+            return callback("The Client Secret was not found!");
+        }
+        const clientId = collector.clientId;
+        if (!clientId) {
+            return callback("The Client ID was not found!");
+        }
+
+        const hostName = collector.pawsDomainEndpoint;
+        if (!clientId) {
+            return callback("The Host Name was not found!");
+        }
+
+        const client = new duo.Client(clientId, clientSecret, hostName);
+
+        var objectDetails = utils.getAPIDetails(state);
+        if (!objectDetails.url) {
+            return callback("The object name was not found!");
+        }
+
+        typeIdPaths = objectDetails.typeIdPaths;
+        tsPaths = objectDetails.tsPaths;
+
+        const stateMaxtime = state.maxtime ? `till ${moment(parseInt(state.maxtime)).toISOString()}` : ``;
+
+        console.info(`CDUO000001 Collecting data for ${state.object} from ${moment(parseInt(state.mintime)).toISOString()} ${stateMaxtime}`);
+
+        utils.getAPILogs(client, objectDetails, state, [], process.env.paws_max_pages_per_invocation)
+            .then(({ accumulator, nextPage }) => {
+                console.log(accumulator.length);
+                console.log(nextPage);
+                let newState;
+                if (nextPage === undefined) {
+                    newState = this._getNextCollectionState(state);
+                } else {
+                    newState = this._getNextCollectionStateWithNextPage(state, nextPage);
+                }
+                console.log(newState);
+                console.info(`CDUO000002 Next collection in ${newState.poll_interval_sec} seconds`);
+                return callback(null, [], newState, newState.poll_interval_sec);
+            }).catch((error) => {
+                return callback(error);
+            });
     }
-    
+
+    _getNextCollectionState(curState) {
+
+        if (curState.object === Authentication) {
+
+            const untilMoment = moment(curState.until);
+
+            const { nextUntilMoment, nextSinceMoment, nextPollInterval } = calcNextCollectionInterval('no-cap', untilMoment, this.pollInterval);
+
+            return {
+                object: curState.object,
+                mintime: nextSinceMoment.valueOf(),
+                maxtime: nextUntilMoment.valueOf(),
+                nextPage: null,
+                poll_interval_sec: nextPollInterval
+            };
+        }
+        else {
+            // This condition works if next page getting null or undefined
+            const untilMoment = moment();
+
+            const { nextUntilMoment, nextSinceMoment, nextPollInterval } = calcNextCollectionInterval('no-cap', untilMoment, this.pollInterval);
+
+            return {
+                object: curState.object,
+                mintime: nextSinceMoment.unix(),
+                poll_interval_sec: nextPollInterval
+            };
+
+        }
+    }
+
+    _getNextCollectionStateWithNextPage(curState, nextPage) {
+
+        if (curState.object === Authentication) {
+            return {
+                object: curState.object,
+                mintime: curState.mintime,
+                maxtime: curState.maxtime,
+                nextPage: nextPage,
+                poll_interval_sec: 1
+            };
+        } else {
+            return {
+                object: curState.object,
+                mintime: nextPage,
+                poll_interval_sec: 1
+            };
+        }
+    }
+
     pawsFormatLog(msg) {
-        // TODO: double check that this message parsing fits your use case
         let collector = this;
 
         const ts = parse.getMsgTs(msg, tsPaths);
         const typeId = parse.getMsgTypeId(msg, typeIdPaths);
-        
+
         let formattedMsg = {
             messageTs: ts.sec,
             priority: 11,
@@ -92,7 +170,7 @@ class CiscoduoCollector extends PawsCollector {
             messageType: 'json/ciscoduo',
             application_id: collector.application_id
         };
-        
+
         if (typeId !== null && typeId !== undefined) {
             formattedMsg.messageTypeId = `${typeId}`;
         }
