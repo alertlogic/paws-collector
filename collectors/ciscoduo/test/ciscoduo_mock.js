13fcd573310ab6cbb43fd643c86a68d67b3bf660
suraj <suraj.kadam@niyuj.com>
2020-06-05 14:41:24 +0530

    Unit test changes...

---
+++ collectors/ciscoduo/test/ciscoduo_mock.js
@@ -0,0 +1,63 @@
+process.env.AWS_REGION = 'us-east-1';
+process.env.al_api = 'api.global-services.global.alertlogic.com';
+process.env.ingest_api = 'ingest.global-services.global.alertlogic.com';
+process.env.azollect_api = 'azcollect.global-services.global.alertlogic.com';
+process.env.aims_access_key_id = 'aims-key-id';
+process.env.aims_secret_key = 'aims-secret-key-encrypted';
+process.env.log_group = 'logGroupName';
+process.env.paws_state_queue_arn = "arn:aws:sqs:us-east-1:352283894008:paws-state-queue";
+process.env.paws_extension = 'ciscoduo';
+process.env.ciscoduo_endpoint = 'https://test.alertlogic.com/';
+process.env.ciscoduo_token = 'ciscoduo-token';
+process.env.collector_id = 'collector-id';
+process.env.al_application_id = 'application_id';
+process.env.paws_secret_param_name = "ciscoduo-param-name";
+process.env.paws_poll_interval = 60;
+process.env.paws_type_name = "ciscoduo";
+process.env.paws_api_secret = "secret";
+process.env.paws_endpoint = "ciscoduo.com";
+process.env.paws_collector_param_string_1 = "[\"Authentication\", \"Administrator\",\"Telephony\", \"OfflineEnrollment\"]";
+process.env.paws_api_client_id = "client-id";
+
+const AIMS_TEST_CREDS = {
+    access_key_id: 'test-access-key-id',
+    secret_key: 'test-secret-key'
+};
+
+const LOG_EVENT = {
+    access_device:
+    {
+        browser: 'Unknown',
+        browser_version: '11.0',
+        flash_version: null,
+        hostname: null,
+        ip: '0.0.0.0',
+        java_version: null,
+        location: [Object],
+        os: 'Windows',
+        os_version: '7'
+    },
+    alias: '',
+    application: { key: 'qweqwewqeqw', name: 'portal' },
+    auth_device: { ip: null, location: [Object], name: null },
+    email: 'demo@demo.com',
+    event_type: 'authentication',
+    factor: null,
+    isotimestamp: '2020-06-03T14:29:17.668359+00:00',
+    reason: 'bypass_user',
+    result: 'success',
+    timestamp: 1591194557,
+    trusted_endpoint_status: 'unknown',
+    txid: 'qwewqewq-23123-adasd-123112d-12323qd',
+    user: { key: 'qweqwewqeqw', name: 'testuser' }
+};
+
+const FUNCTION_ARN = 'arn:aws:lambda:us-east-1:352283894008:function:test-01-CollectLambdaFunction-2CWNLPPW5XO8';
+const FUNCTION_NAME = 'test-TestCollectLambdaFunction-1JNNKQIPOTEST';
+
+module.exports = {
+    AIMS_TEST_CREDS: AIMS_TEST_CREDS,
+    FUNCTION_ARN: FUNCTION_ARN,
+    FUNCTION_NAME: FUNCTION_NAME,
+    LOG_EVENT: LOG_EVENT
+};
---
+++ collectors/ciscoduo/test/ciscoduo_test.js
@@ -0,0 +1,301 @@
+const assert = require('assert');
+const sinon = require('sinon');
+var AWS = require('aws-sdk-mock');
+const m_response = require('cfn-response');
+const ciscoduoMock = require('./ciscoduo_mock');
+var CiscoduoCollector = require('../collector').CiscoduoCollector;
+const moment = require('moment');
+const utils = require("../utils");
+
+
+var responseStub = {};
+let getAPIDetails;
+let getAPILogs;
+
+describe('Unit Tests', function () {
+    beforeEach(function () {
+        AWS.mock('SSM', 'getParameter', function (params, callback) {
+            const data = new Buffer('test-secret');
+            return callback(null, { Parameter: { Value: data.toString('base64') } });
+        });
+        AWS.mock('KMS', 'decrypt', function (params, callback) {
+            const data = {
+                Plaintext: '{}'
+            };
+            return callback(null, data);
+        });
+
+        responseStub = sinon.stub(m_response, 'send').callsFake(
+            function fakeFn(event, mockContext, responseStatus, responseData, physicalResourceId) {
+                mockContext.succeed();
+            });
+    });
+
+    afterEach(function () {
+        responseStub.restore();
+    });
+
+
+    describe('Paws Init Collection State', function () {
+        let ctx = {
+            invokedFunctionArn: ciscoduoMock.FUNCTION_ARN,
+            fail: function (error) {
+                assert.fail(error);
+            },
+            succeed: function () { }
+        };
+        it('Paws Init Collection State Success', function (done) {
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                const startDate = moment().subtract(1, 'days').toISOString();
+                process.env.paws_collection_start_ts = startDate;
+
+                collector.pawsInitCollectionState(null, (err, initialStates, nextPoll) => {
+                    initialStates.forEach((state) => {
+                        if (state.object === "Authentication") {
+                            assert.equal(moment(parseInt(state.maxtime)).diff(parseInt(state.mintime), 'seconds'), 60);
+                        }
+                        else {
+                            assert.equal(state.poll_interval_sec, 1);
+                            assert.ok(state.mintime);
+                        }
+                    });
+                    done();
+                });
+            });
+        });
+    });
+
+    describe('Paws Get Register Parameters', function () {
+        it('Paws Get Register Parameters Success', function (done) {
+            let ctx = {
+                invokedFunctionArn: ciscoduoMock.FUNCTION_ARN,
+                fail: function (error) {
+                    assert.fail(error);
+                    done();
+                },
+                succeed: function () {
+                    done();
+                }
+            };
+
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                const sampleEvent = { ResourceProperties: { StackName: 'a-stack-name' } };
+                collector.pawsGetRegisterParameters(sampleEvent, (err, regValues) => {
+                    const expectedRegValues = {
+                        ciscoduoObjectNames: '[\"Authentication\", \"Administrator\",\"Telephony\", \"OfflineEnrollment\"]',
+                    };
+                    assert.deepEqual(regValues, expectedRegValues);
+                    done();
+                });
+            });
+        });
+    });
+
+    describe('pawsGetLogs', function () {
+        let ctx = {
+            invokedFunctionArn: ciscoduoMock.FUNCTION_ARN,
+            fail: function (error) {
+                assert.fail(error);
+            },
+            succeed: function () { }
+        };
+        it('Paws Get Logs Success', function (done) {
+            getAPILogs = sinon.stub(utils, 'getAPILogs').callsFake(
+                function fakeFn(client, objectDetails, state, accumulator, maxPagesPerInvocation) {
+                    return new Promise(function (resolve, reject) {
+                        return resolve({ accumulator: [ciscoduoMock.LOG_EVENT, ciscoduoMock.LOG_EVENT] });
+                    });
+                });
+            getAPIDetails = sinon.stub(utils, 'getAPIDetails').callsFake(
+                function fakeFn(state) {
+                    const startDate = moment().subtract(3, 'days');
+                    return {
+                        url: "api_url",
+                        typeIdPaths: [{ path: ["txid"] }],
+                        tsPaths: [{ path: ["timestamp"] }],
+                        query: {
+                            mintime: startDate.valueOf(),
+                            maxtime: startDate.add(2, 'days').valueOf(),
+                            limit: 1000
+                        },
+                        method: "GET"
+                    };
+                });
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                const startDate = moment().subtract(3, 'days');
+                const curState = {
+                    object: "Authentication",
+                    mintime: startDate.valueOf(),
+                    maxtime: startDate.add(2, 'days').valueOf(),
+                    nextPage: null,
+                    poll_interval_sec: 1
+                };
+                collector.pawsGetLogs(curState, (err, logs, newState, newPollInterval) => {
+                    assert.equal(logs.length, 2);
+                    assert.equal(newState.poll_interval_sec, 1);
+                    assert.ok(logs[0].txid);
+                    getAPILogs.restore();
+                    getAPIDetails.restore();
+                    done();
+                });
+
+            });
+        });
+
+        it('Paws Get Logs With NextPage Success', function (done) {
+            getAPILogs = sinon.stub(utils, 'getAPILogs').callsFake(
+                function fakeFn(client, objectDetails, state, accumulator, maxPagesPerInvocation) {
+                    return new Promise(function (resolve, reject) {
+                        return resolve({ accumulator: [ciscoduoMock.LOG_EVENT, ciscoduoMock.LOG_EVENT], nextPage: "nextPage" });
+                    });
+                });
+            getAPIDetails = sinon.stub(utils, 'getAPIDetails').callsFake(
+                function fakeFn(state) {
+                    const startDate = moment().subtract(3, 'days');
+                    return {
+                        url: "api_url",
+                        typeIdPaths: [{ path: ["txid"] }],
+                        tsPaths: [{ path: ["timestamp"] }],
+                        query: {
+                            mintime: startDate.valueOf(),
+                            maxtime: startDate.add(2, 'days').valueOf(),
+                            limit: 1000
+                        },
+                        method: "GET"
+                    };
+                });
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                const startDate = moment().subtract(3, 'days');
+                const curState = {
+                    object: "Authentication",
+                    mintime: startDate.valueOf(),
+                    maxtime: startDate.add(2, 'days').valueOf(),
+                    nextPage: null,
+                    poll_interval_sec: 1
+                };
+                collector.pawsGetLogs(curState, (err, logs, newState, newPollInterval) => {
+                    assert.equal(logs.length, 2);
+                    assert.equal(newState.poll_interval_sec, 1);
+                    assert.equal(newState.nextPage, "nextPage");
+                    assert.ok(logs[0].txid);
+                    getAPILogs.restore();
+                    getAPIDetails.restore();
+                    done();
+                });
+
+            });
+        });
+    });
+
+    describe('Next state tests', function () {
+        let ctx = {
+            invokedFunctionArn: ciscoduoMock.FUNCTION_ARN,
+            fail: function (error) {
+                assert.fail(error);
+            },
+            succeed: function () { }
+        };
+
+        it('Next state tests success with Authentication', function (done) {
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                const startDate = moment();
+                const curState = {
+                    object: "Authentication",
+                    mintime: startDate.valueOf(),
+                    maxtime: startDate.add(collector.pollInterval, 'seconds').valueOf(),
+                    nextPage: null,
+                    poll_interval_sec: 1
+                };
+                let nextState = collector._getNextCollectionState(curState);
+                assert.equal(nextState.poll_interval_sec, collector.pollInterval);
+                done();
+            });
+        });
+
+        it('Next state tests success with OfflineEnrollment', function (done) {
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                const startDate = moment();
+                const curState = {
+                    object: "OfflineEnrollment",
+                    mintime: startDate.unix(),
+                    poll_interval_sec: 1
+                };
+                let nextState = collector._getNextCollectionState(curState);
+                assert.equal(nextState.poll_interval_sec, collector.pollInterval);
+                done();
+            });
+        });
+    });
+
+    describe('Format Tests', function () {
+        it('log format success', function (done) {
+            let ctx = {
+                invokedFunctionArn: ciscoduoMock.FUNCTION_ARN,
+                fail: function (error) {
+                    assert.fail(error);
+                    done();
+                },
+                succeed: function () {
+                    done();
+                }
+            };
+
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                let fmt = collector.pawsFormatLog(ciscoduoMock.LOG_EVENT);
+                assert.equal(fmt.progName, 'CiscoduoCollector');
+                assert.ok(fmt.message);
+                done();
+            });
+        });
+    });
+
+    describe('NextCollectionStateWithNextPage', function () {
+        let ctx = {
+            invokedFunctionArn: ciscoduoMock.FUNCTION_ARN,
+            fail: function (error) {
+                assert.fail(error);
+            },
+            succeed: function () { }
+        };
+        it('Get Next Collection State (Authentication) With NextPage Success', function (done) {
+            const startDate = moment().subtract(5, 'minutes');
+            const curState = {
+                object: "Authentication",
+                mintime: startDate.valueOf(),
+                maxtime: startDate.add(5, 'minutes').valueOf(),
+                poll_interval_sec: 1
+            };
+            const nextPage = "nextPage";
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                let nextState = collector._getNextCollectionStateWithNextPage(curState, nextPage);
+                assert.ok(nextState.nextPage);
+                assert.equal(nextState.nextPage, nextPage);
+                done();
+            });
+        });
+        it('Get Next Collection State (OfflineEnrollment) With NextPage Success', function (done) {
+            const startDate = moment().subtract(5, 'minutes');
+            const curState = {
+                object: "OfflineEnrollment",
+                mintime: startDate.unix(),
+                poll_interval_sec: 1
+            };
+            const nextPageTimestamp = "1574157600";
+            CiscoduoCollector.load().then(function (creds) {
+                var collector = new CiscoduoCollector(ctx, creds, 'ciscoduo');
+                let nextState = collector._getNextCollectionStateWithNextPage(curState, nextPageTimestamp);
+                assert.ok(nextState.mintime);
+                assert.equal(nextState.mintime, nextPageTimestamp);
+                done();
+            });
+        });
+    });
+});
