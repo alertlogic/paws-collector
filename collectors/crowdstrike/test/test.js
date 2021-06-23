409cab211ed54a3ab7e9b44158a39ec51769bd3f
Sameer Bagade <sbagade@womply.com>
2021-06-23 13:48:55 +0530

    Initial Commit Crowdstrike

---
+++ collectors/crowdstrike/test/test.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const sinon = require('sinon');
+var AWS = require('aws-sdk-mock');
+const m_response = require('cfn-response');
+
+const crowdstrikeMock = require('./crowdstrike_mock');
+var m_alCollector = require('@alertlogic/al-collector-js');
+var CrowdstrikeCollector = require('../crowdstrike_collector').CrowdstrikeCollector;
+const m_al_aws = require('@alertlogic/al-aws-collector-js').Util;
+
+describe('Unit Tests', function() {
+    describe('Next state tests', function() {
+        it('log format success', function(done) {
+            let ctx = {
+                invokedFunctionArn : crowdstrikeMock.FUNCTION_ARN,
+                fail : function(error) {
+                    assert.fail(error);
+                    done();
+                },
+                succeed : function() {
+                    done();
+                }
+            };
+            
+            CrowdstrikeCollector.load().then(function(creds) {
+                var collector = new CrowdstrikeCollector(ctx, creds, 'crowdstrike');
+                let nextState = collector._getNextCollectionState(crowdstrikeMock.LOG_EVENT);
+                console.log('!!!', fmt);
+                // put some assertions on you next state here
+                done();
+            });
+        });
+    });
+
+    describe('Format Tests', function() {
+        it('log format success', function(done) {
+            let ctx = {
+                invokedFunctionArn : crowdstrikeMock.FUNCTION_ARN,
+                fail : function(error) {
+                    assert.fail(error);
+                    done();
+                },
+                succeed : function() {
+                    done();
+                }
+            };
+            
+            CrowdstrikeCollector.load().then(function(creds) {
+                var collector = new CrowdstrikeCollector(ctx, creds, 'crowdstrike');
+                let fmt = collector.pawsFormatLog(crowdstrikeMock.LOG_EVENT);
+                console.log('!!!', fmt);
+                // put some assertions on your formatted message here
+                done();
+            });
+        });
+    });
+});
