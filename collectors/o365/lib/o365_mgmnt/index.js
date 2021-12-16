bbd23b15aba669f78fbd2f2bd9860cf71d5eadff
rakhi.mundhada <rakhi.mundhada@alertlogic.com>
2021-12-16 15:41:04 +0530

    Added the custom validation

--- collectors/o365/lib/o365_mgmnt/index.js
+++ collectors/o365/lib/o365_mgmnt/index.js
@@ -15,14 +15,17 @@ const m_o365mgmnt = require('./o365management');
 const {ApplicationTokenCredentials} = require('@azure/ms-rest-nodeauth');
 
 function getO365ManagmentClient(creds) {
-    const AzureTenantId = process.env.paws_collector_param_string_1;
-    var g_appAdCreds = new ApplicationTokenCredentials(
-        creds.client_id,
-        AzureTenantId,
-        creds.secret,
-        'https://manage.office.com'
-    );
-    return new m_o365mgmnt.O365Management(g_appAdCreds, AzureTenantId);
+
+    if (creds.secret && creds.client_id) {
+        const AzureTenantId = process.env.paws_collector_param_string_1;
+        var g_appAdCreds = new ApplicationTokenCredentials(
+            creds.client_id,
+            AzureTenantId,
+            creds.secret,
+            'https://manage.office.com'
+        );
+        return new m_o365mgmnt.O365Management(g_appAdCreds, AzureTenantId);
+    } else throw new Error("client secret or client id must be a non empty string.");
 }
 
 /**
