fcfac8ddcc1d63a22ee7c0a84871f2e54e0f5ca7
imranalisyed506 <105209301+imranalisyed506@users.noreply.github.com>
2022-06-09 15:00:16 +0530

    Mimecast download zip logs changes (#286)
    * mimecast download zip logs json from api changes

--- collectors/mimecast/test/utils_test.js
+++ collectors/mimecast/test/utils_test.js
@@ -4,13 +4,14 @@ const sinon = require('sinon');
 const mimecastMock = require('./mimecast_mock');
 var request = require('request');
 const moment = require('moment');
-
+const path = require('path');
+const TEST_ZIP_PATH = path.join(__dirname, 'test1.zip');
 var alserviceStub = {};
 
 describe('Unit Tests', function () {
     describe('Get API Logs (AttachmentProtectLogs)', function () {
         it('Get API Logs (AttachmentProtectLogs) success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null,{},JSON.stringify({ fail: [], data: [{ attachmentLogs: [mimecastMock.LOG_EVENT] }], meta: { pagination: {} } }));
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, {}, JSON.stringify({ fail: [], data: [{ attachmentLogs: [mimecastMock.LOG_EVENT] }], meta: { pagination: {} } }));
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             const startDate = moment().subtract(5, 'days');
@@ -38,7 +39,7 @@ describe('Unit Tests', function () {
 
     describe('Get API Logs (AttachmentProtectLogs) with nextpage', function () {
         it('Get API Logs (AttachmentProtectLogs) with nextpage success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null,{},JSON.stringify({ fail: [], data: [{ attachmentLogs: [mimecastMock.LOG_EVENT] }], meta: { pagination: { next: "next" } } }));
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, {}, JSON.stringify({ fail: [], data: [{ attachmentLogs: [mimecastMock.LOG_EVENT] }], meta: { pagination: { next: "next" } } }));
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             const startDate = moment().subtract(5, 'days');
@@ -66,7 +67,7 @@ describe('Unit Tests', function () {
 
     describe('Get API Logs (URLProtectLogs)', function () {
         it('Get API Logs (URLProtectLogs) success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null,{},JSON.stringify({ fail: [], data: [{ clickLogs: [mimecastMock.URL_PROTECT_LOGS_EVENT] }], meta: { pagination: {} } }));
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, {}, JSON.stringify({ fail: [], data: [{ clickLogs: [mimecastMock.URL_PROTECT_LOGS_EVENT] }], meta: { pagination: {} } }));
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             const startDate = moment().subtract(5, 'days');
@@ -94,7 +95,7 @@ describe('Unit Tests', function () {
 
     describe('Get API Logs (URLProtectLogs) with nextpage', function () {
         it('Get API Logs (URLProtectLogs) with nextpage success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null,{},JSON.stringify({ fail: [], data: [{ clickLogs: [mimecastMock.URL_PROTECT_LOGS_EVENT] }], meta: { pagination: { next: "next" } } }));
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, {}, JSON.stringify({ fail: [], data: [{ clickLogs: [mimecastMock.URL_PROTECT_LOGS_EVENT] }], meta: { pagination: { next: "next" } } }));
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             const startDate = moment().subtract(5, 'days');
@@ -122,7 +123,7 @@ describe('Unit Tests', function () {
 
     describe('Get API Logs (MalwareFeed) with no logs', function () {
         it('Get API Logs (MalwareFeed) with no logs success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "headers": {} },JSON.stringify({ id: "bundle--bf8be578-3953-4b80-ae84-312d149b91e8", objects: [] } ));
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "headers": {} }, JSON.stringify({ id: "bundle--bf8be578-3953-4b80-ae84-312d149b91e8", objects: [] }));
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             let state = {
@@ -147,7 +148,7 @@ describe('Unit Tests', function () {
 
     describe('Get API Logs (MalwareFeed)', function () {
         it('Get API Logs (MalwareFeed) success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "headers": { "x-mc-threat-feed-next-token": "token" } },JSON.stringify({ id: "bundle--bf8be578-3953-4b80-ae84-312d149b91e8", objects: [mimecastMock.MALWARE_FEED_LOGS_EVENT] } ));
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "headers": { "x-mc-threat-feed-next-token": "token" } }, JSON.stringify({ id: "bundle--bf8be578-3953-4b80-ae84-312d149b91e8", objects: [mimecastMock.MALWARE_FEED_LOGS_EVENT] }));
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             let state = {
@@ -175,7 +176,8 @@ describe('Unit Tests', function () {
             alserviceStub.post = sinon.stub(request, 'post').yields(null, {
                 "meta": {
                     "isLastToken": true
-                }},JSON.stringify({ "data": [] }));
+                }
+            }, JSON.stringify({ "data": [] }));
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             let state = {
@@ -198,9 +200,9 @@ describe('Unit Tests', function () {
         });
     });
 
-    describe('Get API Logs (SiemLogs)', function () {
-        it('Get API Logs (SiemLogs) with nextpage success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "headers": { "mc-siem-token": "token" } },JSON.stringify(mimecastMock.SIEM_LOGS_EVENT));
+    describe('Get API Logs (SiemLogs) Download zip file', function () {
+        it('Get API Logs (SiemLogs) extract zip buffer and accumulate logs from json files', function (done) {
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "headers": { "mc-siem-token": "token" } }, TEST_ZIP_PATH);
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             let state = {
@@ -216,16 +218,51 @@ describe('Unit Tests', function () {
                 "appKey": "appKey"
             };
             utils.getAPILogs(authDetails, state, accumulator, maxPagesPerInvocation).then(data => {
-                assert(accumulator.length == 5, "accumulator length is wrong");
+                console.log(accumulator);
+                assert(accumulator.length ===0, "accumulator length is wrong");
                 alserviceStub.post.restore();
                 done();
             });
         });
     });
 
+    describe('Get API Logs (SiemLogs) Download zip file failed', function () {
+        it('Get API Logs (SiemLogs) extract zip buffer failed', function (done) {
+            // const unzipBufferSpy = sinon.spy(utils.unzipBuffer);
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "headers": { "mc-siem-token": "token" } }, JSON.stringify(mimecastMock.SIEM_LOGS_EVENT));
+            let maxPagesPerInvocation = 5;
+            let accumulator = [];
+            let state = {
+                stream: "SiemLogs",
+                nextPage: null,
+                poll_interval_sec: 1
+            };
+            let authDetails = {
+                "baseUrl": "baseUrl",
+                "accessKey": "accessKey",
+                "secretKey": "secretKey",
+                "appId": "appId",
+                "appKey": "appKey"
+            };
+            let errMessage = 'Invalid filename';
+            utils.getAPILogs(authDetails, state, accumulator, maxPagesPerInvocation).then(data => {
+                // unzipBufferSpy(path.join(__dirname, "./test1.zip")).then(function (bodyData) {
+                //     accumulator.push(...bodyData);
+                // }).catch((error) => {
+                // });
+                assert(accumulator.length == 0, "accumulator length is wrong");
+                assert(errMessage === 'Invalid filename', "Returned Error message");
+                // assert(unzipBufferSpy.called, "unzipBuffer method not called");
+                alserviceStub.post.restore();
+                done();
+
+            });
+        });
+    });
+
     describe('Get API Logs (MalwareFeed)', function () {
         it('Get API Logs (MalwareFeed) with Api Throttling success', function (done) {
-            alserviceStub.post = sinon.stub(request, 'post').yields(null,{ "statusCode": 429 },"The Mimecast service you're trying to access is temporarily busy. Please try again in a few minutes and then contact your IT helpdesk if you still have problems.");
+            alserviceStub.post = sinon.stub(request, 'post').yields(null, { "statusCode": 429 }, "The Mimecast service you're trying to access is temporarily busy. Please try again in a few minutes and then contact your IT helpdesk if you still have problems.");
             let maxPagesPerInvocation = 5;
             let accumulator = [];
             let state = {
@@ -246,5 +283,65 @@ describe('Unit Tests', function () {
                 done();
             });
         });
+    });
+
+    describe('Get getTypeIdAndTsPaths', function () {
+        it('Get getTypeIdAndTsPaths SiemLogs success', function (done) {
+            let state = {
+                stream: "SiemLogs",
+                nextPage: null,
+                poll_interval_sec: 1
+            };
+            const typeIdPaths = [{ path: ["aCode"] }];
+            const tsPaths = [{ path: ["datetime"] }];
+            let typeIdAndTsPaths = utils.getTypeIdAndTsPaths(state.stream);
+
+            assert.equal(typeIdAndTsPaths.typeIdPaths.path === typeIdPaths.path, true);
+            assert.equal(typeIdAndTsPaths.tsPaths.path === tsPaths.path, true);
+            done();
+        });
+        it('Get getTypeIdAndTsPaths Attachment_Protect_Logs success', function (done) {
+            let state = {
+                stream: "AttachmentProtectLogs",
+                nextPage: null,
+                poll_interval_sec: 1
+            };
+            const typeIdPaths = [{ path: ["definition"] }];
+            const tsPaths = [{ path: ["date"] }];
+            let typeIdAndTsPaths = utils.getTypeIdAndTsPaths(state.stream);
+            assert.equal(typeIdAndTsPaths.typeIdPaths.path === typeIdPaths.path, true);
+            assert.equal(typeIdAndTsPaths.tsPaths.path === tsPaths.path, true);
+            done();
+        });
+
+        it('Get getTypeIdAndTsPaths URLProtectLogs success', function (done) {
+            let state = {
+                stream: "URLProtectLogs",
+                nextPage: null,
+                poll_interval_sec: 1
+            };
+            const typeIdPaths = [{ path: ["category"] }];
+            const tsPaths = [{ path: ["date"] }];
+            let typeIdAndTsPaths = utils.getTypeIdAndTsPaths(state.stream);
+            assert.equal(typeIdAndTsPaths.typeIdPaths.path === typeIdPaths.path, true);
+            assert.equal(typeIdAndTsPaths.tsPaths.path === tsPaths.path, true);
+            done();
+        });
+
+        it('Get getTypeIdAndTsPaths Malware_Feed success', function (done) {
+            let state = {
+                stream: "MalwareFeed",
+                nextPage: null,
+                poll_interval_sec: 1
+            };
+            const typeIdPaths = [{ path: ["type"] }];
+            const tsPaths = [{ path: ["created"] }];
+            let typeIdAndTsPaths = utils.getTypeIdAndTsPaths(state.stream);
+            assert.equal(typeIdAndTsPaths.typeIdPaths.path === typeIdPaths.path, true);
+            assert.equal(typeIdAndTsPaths.tsPaths.path === tsPaths.path, true);
+            done();
+        });
+
+
     });
 });
