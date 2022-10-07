cc7c192a888ef6d6fa9292be1a2e8f3e034095b4
Rakhi <rakhi@INPNQLT045.local>
2022-10-07 16:54:12 +0530

    Added check if date is not null

--- collectors/ciscoamp/utils.js
+++ collectors/ciscoamp/utils.js
@@ -33,8 +33,8 @@ function getAPILogs(baseUrl, authorization, apiUrl, state, accumulator, maxPages
                     }
                     else {
                         if (state.stream === Events && accumulator.length > 0) {
-                            const newDate = accumulator[0].date ? accumulator[0].date : accumulator[1].date ? accumulator[1].date : accumulator[2].date;
-                            newSince = moment(newDate).add(1, 'seconds');
+                            const findDate = accumulator[0].date ? accumulator[0].date : accumulator[1].date ? accumulator[1].date : accumulator[2].date;
+                            newSince = moment(findDate).add(1, 'seconds');
                             if (!newSince) {
                                 reject(`CAMP000005 Date is not available in Events api response`);
                             }
