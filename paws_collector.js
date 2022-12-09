fcdd459bc2c3b90791e7f00674624868ddccc9b6
Rakhi <rakhi@INPNQLT045.local>
2022-12-09 12:10:44 +0530

    Added extra parameter to identify the message from which collector

--- paws_collector.js
+++ paws_collector.js
@@ -686,6 +686,13 @@ class PawsCollector extends AlAwsCollector {
     };
 
     _storeCollectionState(pawsState, privCollectorState, invocationTimeout, callback) {
+        // Added extra parameter to identify the message from which collector in case it failed and went into DLQ
+        let extraParam = {
+            lambda_function_name: process.env.AWS_LAMBDA_FUNCTION_NAME,
+            customer_id: process.env.customer_id,
+            account_id: this._awsAccountId
+        };
+        pawsState = Object.assign(pawsState, extraParam);
         if (Array.isArray(privCollectorState)) {
             return this._storeCollectionStateArray(pawsState, privCollectorState, invocationTimeout, callback);
         } else {
