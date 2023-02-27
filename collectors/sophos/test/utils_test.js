2d7fa17186a8058e660917e888ff4a8121ed1f91
Pranav-Arya37 <pranav.arya@alertlogic.com>
2023-02-27 13:47:39 +0530

    added testcases for sophos collector

--- collectors/sophos/test/utils_test.js
+++ collectors/sophos/test/utils_test.js
@@ -36,6 +36,37 @@ describe('Unit Tests', function () {
         });
     });
 
+    describe('Get API Logs with Error', function () {
+        it('Get API Logs with Error', function (done) {
+            alserviceStub.get = sinon.stub(RestServiceClient.prototype, 'get').callsFake(
+                function fakeFn() {
+                    return new Promise(function (resolve, reject) {
+                        return reject({ statusCode: 503,
+                            error: "Service Unavailable",
+                           message: "The API service is temporarily unavailable, please try again later"
+                        });
+                    });
+                });
+            let maxPagesPerInvocation = 5;
+            let accumulator = [];
+            let token = "token";
+            let tenant_Id = "57ca9a6b-885f-4e36-95ec-290548c26059";
+            const baseUrl = "api-us03.central.sophos.com";
+            const startDate = moment().subtract(3, 'days');
+            const state = {
+                since: startDate.toISOString(),
+                until: startDate.add(2, 'days').toISOString(),
+                nextPage: null,
+                poll_interval_sec: 1
+            };
+            utils.getAPILogs(baseUrl, token, tenant_Id, state, accumulator, maxPagesPerInvocation).catch(err => {
+                assert.equal(err.message, "The API service is temporarily unavailable, please try again later", "Error message is not correct");
+                alserviceStub.get.restore();
+                done();
+            });
+        });
+    });
+
     describe('Get API Logs with nextPage', function () {
         it('Get API Logs with nextPage success', function (done) {
             alserviceStub.get = sinon.stub(RestServiceClient.prototype, 'get').callsFake(
