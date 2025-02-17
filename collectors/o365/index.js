4319316bec7689836a579c0e7a4c7e6f051e337c
imranalisyed506 <105209301+imranalisyed506@users.noreply.github.com>
2025-02-17 10:25:57 +0530

    o365 initial commit with dockerfiles (#387)

--- collectors/o365/index.js
+++ collectors/o365/index.js
@@ -8,15 +8,56 @@
  * -----------------------------------------------------------------------------
  */
 
-const debug = require('debug') ('index');
+const debug = require('debug')('index');
 const AlLogger = require('@alertlogic/al-aws-collector-js').Logger;
 
 const O365Collector = require('./o365_collector').O365Collector;
+const express = require('express');
 
-exports.handler = O365Collector.makeHandler(function(event, context) {
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const handleEventRequest = (req, res) => {
+    require('dotenv').config({ path: __dirname + '/local/.env' })
+    const event = req.body;
+    const context = { awsRequestId: 1, invokedFunctionArn: "arn:aws:lambda:us-east-1:352283894008:function:imo365newfromlocal1" }
+
     debug('input event: ', event);
-    AlLogger.defaultMeta = {requestId: context.awsRequestId};
-    O365Collector.load().then(function(creds) {
+    AlLogger.defaultMeta = { requestId: context.awsRequestId };
+    O365Collector.load().then(function (creds) {
+        var o365c = new O365Collector(context, creds);
+        o365c.handleEvent(event);
+    }).catch(error => {
+        AlLogger.error(`O365000006 error in creating object ${error}`);
+        return error;
+    });
+};
+
+app.post('/event', handleEventRequest);
+
+setInterval(() => {
+    let messageId = require('crypto').randomUUID()
+    const fs = require('fs');
+
+    // Read local/events/event_poll.json file and set to collect poll event
+    let rawData = fs.readFileSync('local/events/event_poll.json');
+    let event = { "body": JSON.parse(rawData) };
+    event.body.Records[0].messageId = messageId;
+    console.log(JSON.stringify(event,0,2));
+    handleEventRequest(event)
+}, 10000)
+
+app.listen(port, () => {
+    console.log(`Paws app listening on port ${port}`);
+});
+
+exports.handler = O365Collector.makeHandler(function (event, context) {
+    debug('input event: ', event);
+    AlLogger.defaultMeta = { requestId: context.awsRequestId };
+    O365Collector.load().then(function (creds) {
         var o365c = new O365Collector(context, creds);
         o365c.handleEvent(event);
     }).catch(error => {
