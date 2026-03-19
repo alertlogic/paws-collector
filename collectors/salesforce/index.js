e2bbc81d84a682b6f31fdb10d88aac0bc23e7b26
Rakhi Mundhada <rakhi.mundhada@rakhi.mundhada-C02G237CMD6M>
2026-03-19 14:34:54 +0530

    Migrate the salesforce collector from callback to promise

--- collectors/salesforce/index.js
+++ collectors/salesforce/index.js
@@ -12,11 +12,15 @@ const debug = require('debug') ('index');
 const AlLogger = require('@alertlogic/al-aws-collector-js').Logger;
 const SalesforceCollector = require('./collector').SalesforceCollector;
 
-exports.handler = SalesforceCollector.makeHandler(function(event, context) {
+exports.handler = SalesforceCollector.makeHandler(async function(event, context) {
     debug('input event: ', event);
     AlLogger.defaultMeta = { requestId: context.awsRequestId };
-    SalesforceCollector.load().then(function(creds) {
-        var salesforcec = new SalesforceCollector(context, creds);
-        salesforcec.handleEvent(event);
-    });
+    try {
+        const creds = await SalesforceCollector.load();
+        let salesforcec = new SalesforceCollector(context, creds);
+        await salesforcec.handleEvent(event);
+    } catch (error) {
+        AlLogger.error('Unhandled error in handler: ', error);
+        throw error;
+    }
 });
